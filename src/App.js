e59e8d742a627e05b0f2e19d653a061108a9a205
Dnyanesh Tarte <tartednyanesh@gmail.com>
2020-07-26 19:05:10 -0500

    End of day changes in development. Created half of the components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
+import Header from './Components/Header';
+import Balance from './Components/Balance';
+import IncomeExpense from './Components/IncomeExpense';
 
 function App() {
   return (
-    <div className="App">
-      hello
+    <div>
+      <Header/>
+        <Balance/>
+      <IncomeExpense/>
+      
     </div>
   );
 }
